1231e9e1ed075628dcf84e9b3ccd8f038e3dce09
eydomj <eydomj@gmail.com>
2019-07-23 12:39:56 +0300

    task_2

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+
+def test(request):
+    context = {
+        'msg': 'Hello',
+    }
+    return render(request, "hello.html", context)
+
 # Create your views here.
